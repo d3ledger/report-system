968c4aab651b9324200c528cccd3a1a19da7ac9b
iurii.vinogradov <discours@list.ru>
2019-04-08 16:29:04 +0300

    - Creating template

--- data-collector/src/main/resources/liquibase/changelock_primary_key.sql
+++ data-collector/src/main/resources/liquibase/changelock_primary_key.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 
 --changeset yvinogradov:alter_databasechange_lock_create_id
-ALTER TABLE DATABASECHANGELOG ADD COLUMN primary_key BIGINT PRIMARY KEY AUTO_INCREMENT;
+ALTER TABLE DATABASECHANGELOG ADD COLUMN primary_key BIGSERIAL PRIMARY KEY;
