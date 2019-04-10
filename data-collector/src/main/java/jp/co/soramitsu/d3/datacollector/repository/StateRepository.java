6cf3825fba82d142351ae91940c64fdc2b33b8b2
iurii.vinogradov <discours@list.ru>
2019-04-10 13:15:39 +0300

    - Billing Persistence test

--- data-collector/src/main/java/jp/co/soramitsu/d3/datacollector/repository/StateRepository.java
+++
@@ -1,8 +0,0 @@
-package jp.co.soramitsu.d3.datacollector.repository;
-
-import jp.co.soramitsu.d3.datacollector.model.State;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StateRepository extends CrudRepository<State, Long> {
-
-}
