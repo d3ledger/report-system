8ca1e4be34c42c14b742dac077afda077887e19b
iurii.vinogradov <discours@list.ru>
2019-04-12 10:42:45 +0300

    - pull request changes

--- data-collector/src/main/kotlin/com/d3/datacollector/service/RabbitMqServiceImpl.kt
+++ data-collector/src/main/kotlin/com/d3/datacollector/service/RabbitMqServiceImpl.kt
@@ -5,7 +5,6 @@ import com.d3.datacollector.model.BillingMqDto
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
 
 class RabbitMqServiceImpl : RabbitMqService {
 
