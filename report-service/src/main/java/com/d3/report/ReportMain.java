67500e20a277bd941f8768b77230c55cd67b9cf8
iurii.vinogradov <discours@list.ru>
2019-04-26 14:50:30 +0300

    - Pull request adjustements

--- report-service/src/main/java/com/d3/report/ReportMain.java
+++ report-service/src/main/java/com/d3/report/ReportMain.java
@@ -2,8 +2,6 @@ package com.d3.report;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
