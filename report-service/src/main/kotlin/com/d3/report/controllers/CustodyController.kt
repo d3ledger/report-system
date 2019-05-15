3979bdae489f94b03539d286b8b81770418a876a
iurii.vinogradov <discours@list.ru>
2019-05-15 13:21:30 +0300

    - Cros Origin for frontend

--- report-service/src/main/kotlin/com/d3/report/controllers/CustodyController.kt
+++ report-service/src/main/kotlin/com/d3/report/controllers/CustodyController.kt
@@ -13,6 +13,7 @@ import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -20,6 +21,7 @@ import java.util.stream.Collectors
 import javax.transaction.Transactional
 import javax.validation.constraints.NotNull
 
+@CrossOrigin(maxAge = 3600)
 @Controller
 @RequestMapping("/report/billing/custody")
 class CustodyController {
--- report-service/src/main/kotlin/com/d3/report/controllers/RegisteredAccountsController.kt
+++ report-service/src/main/kotlin/com/d3/report/controllers/RegisteredAccountsController.kt
@@ -14,12 +14,14 @@ import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import java.util.stream.Collectors
 import javax.validation.constraints.NotNull
 
+@CrossOrigin(maxAge = 3600)
 @Controller
 @RequestMapping("/report/billing/registeredAccounts")
 class RegisteredAccountsController {
