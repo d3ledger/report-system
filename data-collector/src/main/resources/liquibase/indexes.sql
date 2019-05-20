4ad40ffe630fc2910fe2bd588274ba330d301f2d
iurii.vinogradov <discours@list.ru>
2019-05-20 11:00:04 +0300

    - Refactoring - Sql Index Command - accountName

--- data-collector/src/main/resources/liquibase/indexes.sql
+++ data-collector/src/main/resources/liquibase/indexes.sql
@@ -1,4 +1,7 @@
 --liquibase formatted sql
 
 --changeset yvinogradov:create_command_account_id_index
-CREATE INDEX  account_by_id  ON command (account_name);
+CREATE INDEX  command_by_name  ON command (account_name);
+CREATE INDEX  command_by_id  ON command (account_id);
+CREATE INDEX  command_by_accountid_and_assetid  ON command (account_id, asset_id);
+CREATE INDEX  command_by_domainid  ON command (domain_id);
--- report-service/src/main/kotlin/com/d3/report/repository/TransferAssetRepo.kt
+++ report-service/src/main/kotlin/com/d3/report/repository/TransferAssetRepo.kt
@@ -35,9 +35,9 @@ interface TransferAssetRepo : CrudRepository<TransferAsset, Long?> {
     fun getDataBetween(accountId:String, billingAccTemplate:String, from: Long, to: Long, pageable: Pageable): Page<TransferAsset>
 
     @Query("SELECT t FROM TransferAsset t WHERE t.transaction.rejected = false" +
-            " and exists (SELECT ta FROM TransferAsset ta WHERE ta.transaction.id = t.transaction.id and ta.destAccountId = :billingAccTemplate)" +
+            " and exists (SELECT ta FROM TransferAsset ta WHERE ta.transaction.id = t.transaction.id and ta.destAccountId = :billingAccount)" +
             " and t.srcAccountId = :accountId" +
             " and t.assetId = :assetId" +
             " and t.transaction.block.blockCreationTime Between :from and :to")
-    fun getDataBetweenForAsset(assetId:String, accountId:String, billingAccTemplate:String, from: Long, to: Long, pageable: Pageable): Page<TransferAsset>
+    fun getDataBetweenForAsset(assetId:String, accountId:String, billingAccount:String, from: Long, to: Long, pageable: Pageable): Page<TransferAsset>
 }
