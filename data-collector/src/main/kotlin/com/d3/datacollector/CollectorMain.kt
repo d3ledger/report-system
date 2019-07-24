f874e6d69e6b8f974da4e961c4321f8ae0b94620
Mingela <freekilluau@gmail.com>
2019-07-24 17:48:13 +0300

    Main in Kotlin

---
+++ data-collector/src/main/kotlin/com/d3/datacollector/CollectorMain.kt
@@ -0,0 +1,15 @@
+package com.d3.datacollector
+
+import com.d3.datacollector.service.BlockTaskService
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@SpringBootApplication
+@EnableSwagger2
+class CollectorMain
+
+fun main(args: Array<String>) {
+    val applicationContext = SpringApplication.run(CollectorMain::class.java, *args)
+    applicationContext.getBean(BlockTaskService::class.java).runService()
+}
