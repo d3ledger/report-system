08c0ce33b83f6dafcb8587f6476f62a9cea2d902
dolgopolovwork <37849469+dolgopolovwork@users.noreply.github.com>
2019-12-03 13:03:10 +0300

    Use proper sql type for "amount" (#73)

--- data-collector/src/main/resources/liquibase/change_eth_proofs_table.sql
+++
@@ -1,6 +0,0 @@
---liquibase formatted sql
-
---changeset dolgopolov:change_eth_proofs_table.sql
-ALTER TABLE eth_withdrawal_proof RENAME COLUMN time TO tx_time;
-ALTER TABLE eth_withdrawal_proof ADD COLUMN block_num bigint NOT NULL;
-ALTER TABLE eth_withdrawal_proof ADD COLUMN tx_index int NOT NULL;
