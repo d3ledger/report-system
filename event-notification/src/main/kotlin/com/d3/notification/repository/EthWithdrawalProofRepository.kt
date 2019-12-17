810fc98868d01d5fd47a0487c178f5772026cb46
dolgopolovwork <37849469+dolgopolovwork@users.noreply.github.com>
2019-12-17 14:40:07 +0300

    Simplify proof query (#86)

--- event-notification/src/main/kotlin/com/d3/notification/repository/EthWithdrawalProofRepository.kt
+++ event-notification/src/main/kotlin/com/d3/notification/repository/EthWithdrawalProofRepository.kt
@@ -1,21 +1,13 @@
 package com.d3.notification.repository
 
 import com.d3.notification.domain.EthWithdrawalProofs
-import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
-import org.springframework.transaction.annotation.Transactional;
 
 interface EthWithdrawalProofRepository : CrudRepository<EthWithdrawalProofs, String> {
 
-    @Query("SELECT e FROM EthWithdrawalProofs e WHERE e.accountIdToNotify=:accountId AND e.blockNum>=:sinceBlockNum AND e.ack=false ORDER BY e.txTime")
-    fun getNoAckProofsByAccount(
-        accountId: String,
-        sinceBlockNum: Long
+    @Query("SELECT e FROM EthWithdrawalProofs e WHERE e.accountIdToNotify=:accountId ORDER BY e.txTime")
+    fun getProofsByAccount(
+        accountId: String
     ): List<EthWithdrawalProofs>
-
-    @Transactional
-    @Modifying
-    @Query("UPDATE EthWithdrawalProofs SET ack=true WHERE id=:proofEventId")
-    fun ackProofByEventId(proofEventId: String)
 }
